08625e030a037070e0b84cba9d43866f068ec453
Pierre-D-G <gamil.pierre@gmail.com>
2017-07-18 12:46:07 -0400

    Created base project folder structure

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+// Get our API routes
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+
+
+app.get('*', (req, res) => {
+  res.status(200).send({message: "Welcome to the Reduxstagram API"})
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+module.exports = app 
